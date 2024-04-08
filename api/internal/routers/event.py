273844fee9ebcc5976de7461e7dad644fc9af48c
Parcurcik <pavelbezborodov@bk.ru>
2024-04-08 09:23:00 +0300

    make methods for routes

--- api/internal/routers/event.py
+++ api/internal/routers/event.py
@@ -9,7 +9,6 @@ from api.dependencies import get_session
 from api.configuration.database import Session
 from api.utils.types import ResponseType
 
-
 site_router = APIRouter(
     prefix='/exposition',
     tags=['exposition'])
@@ -30,9 +29,50 @@ swagger_responses = {
     responses=swagger_responses,
 )
 async def get_event_by_id(
-    event_id: PositiveInt = Path(..., description='The identifier of event to get'),
-    session: Session = Depends(get_session),
+        event_id: PositiveInt = Path(..., description='The identifier of event to get'),
+        session: Session = Depends(get_session),
 ) -> ResponseType:
-    """Get company by identifier"""
-    event = await Event.check_existence(session, event_id)
+    """Get event by identifier"""
+    event = await EventORM.check_existance(session, event_id)
     return event
+
+
+@site_router.delete(
+    '/event/{event_id}',
+    status_code=204,
+    responses=swagger_responses,
+)
+async def delete_event_by_id(
+        event_id: PositiveInt = Path(..., description='The identifier of event'),
+        session: Session = Depends(get_session),
+) -> ResponseType:
+    """Delete event by identifier"""
+    event = await EventORM.check_existance(session, event_id)
+    try:
+        await session.delete(event)
+        await session.commit()
+    except Exception as err:
+        raise err
+
+
+@site_router.post(
+    '/event/{event_id}',
+    status_code=201,
+    responses=swagger_responses,
+)
+async def create_event(
+        session: Session = Depends(get_session),
+) -> ResponseType:
+    pass
+
+
+@site_router.patch(
+    '/application/{application_id}',
+    response_model=schemas.MTKApplicationGet,
+    responses=swagger_responses,
+)
+async def update_application_by_id(
+        event_id: PositiveInt = Path(..., description='The identifier of event to update'),
+        session: Session = Depends(get_session),
+) -> ResponseType:
+    pass
