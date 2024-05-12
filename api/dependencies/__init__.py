d0addf44da66710f013d853ccbb8fb691022efa7
Parcurcik <pavelbezborodov@bk.ru>
2024-05-12 22:17:08 +0500

    fix restart error

--- api/dependencies/__init__.py
+++ api/dependencies/__init__.py
@@ -1,9 +1,11 @@
 from .db import get_session
 from .file import get_image_with
+from .route import memorize_request_body
 
 __all__ = (
     # db
-    'get_session'
+    'get_session',
     # image
-    'get_image_with'
+    'get_image_with',
+    'memorize_request_body',
 )
---
+++ api/routers/__init__.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, FastAPI
+from fastapi.routing import APIRouter
+
+from api.configuration.config import settings as app_settings
+from api.dependencies import memorize_request_body
+from api.routers import (
+    event
+)
+
+
+def init(app: FastAPI) -> None:
+    router = APIRouter(prefix=app_settings.PREFIX, dependencies=[Depends(memorize_request_body)])
+    modules = [
+        event
+    ]
+    for module in modules:
+        router.include_router(module.site_router)
+    app.include_router(router)
+
+
+__all__ = ('init',)
