ab86fbb2710df670644fa4fc5c530b2f49173282
Parcurcik <pavelbezborodov@bk.ru>
2024-05-12 22:16:12 +0500

    del unused files

--- api/configuration/api_answers.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-from dataclasses import dataclass
-
-
-@dataclass
-class ServerSetup:
-    auth_failed_ip: str = 'Неверный ip сервера'
-    auth_failed_pass: str = 'Неверный пароль от сервера'
--- api/internal/events/shutdown.py
+++
@@ -1,5 +0,0 @@
-from api.configuration.database import disconnect_db
-
-
-async def shutdown() -> None:
-    await disconnect_db()
